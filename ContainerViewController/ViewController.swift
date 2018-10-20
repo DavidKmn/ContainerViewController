ba46bd6fc58a7c984b5c715520fecc58e14ccbac
DavidKmn <davidk.9889@gmail.com>
2018-10-20 09:58:54 +0100

    Initial Commit

---
+++ ContainerViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContainerViewController
+//
+//  Created by David on 20/10/2018.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
